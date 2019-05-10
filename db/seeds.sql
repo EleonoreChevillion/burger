409233afcde7d4f6834b2b9df98518e730cfce2c
tomspalding <tspalding@effectual.com>
2019-05-09 21:09:40 -0400

    adding app

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Mushroom burger");
+
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Bacon double cheese burger");
+
+INSERT INTO burgers_table
+    (burger_name)
+VALUES("Blue cheese burger");
